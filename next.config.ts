4ad2bc5ee5fc355698cfc2aac7fd222d196ec6ae
Minyoung Seol <seol.ca06@gmail.com>
2025-08-26 18:05:33 -0400

    feat: add HTTPS support, security headers, and update .gitignore
    - Enabled HTTPS in the Next.js application for consistent local and production environments.
    - Updated .gitignore to exclude local credentials directory.
    
    Added security headers in next.config.ts:
    - X-Content-Type-Options: prevent MIME type sniffing
    - X-Frame-Options: prevent clickjacking
    - Strict-Transport-Security: enforce HTTPS
    - Referrer-Policy: limit referrer information
    - Content-Security-Policy: restrict content sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  async headers() {
+    return[
+      
+      {
+
+        source : "/(.*)",
+        headers : [
+          {key: 'X-Content-Type-Options', value: 'nosniff'}, // Prevents MIME type sniffing -> ease script/style injection attacks
+          {key: 'X-Frame-Options', value: 'DENY'}, // Prevents Clickjacking attacks. Sets page loading in iframe
+          {key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload'}, // HSTS: Enforces secure connections
+          {key: 'Referrer-Policy', value: 'same-origin'}, // minimize referrer info leakage
+          {key: 'Content-Security-Policy', value: "default-src 'self'"}, // CSP: Prevents XSS, injection attacks. Limits external src permission scope
+        ]
+
+      }
+
+    ]
+  },
+
 };
 
 export default nextConfig;
