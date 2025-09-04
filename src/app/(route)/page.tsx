48de974619abaf45aec62f196465b4e273573fa0
MinyoungSeol <seol.ca06@gmail.com>
2025-09-04 18:34:24 -0400

    refactor: minor text edit to test TailWindCSS in page.tsx

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -10,14 +10,11 @@ export default function Home() {
   const content = lang === 'en' ? en : ko;
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <main className = "mx-auto flex min-h-screen max-w-4xl flex-col items-center justify-center px-4">
 
-        <h1>{content.greeting}</h1>
-        <p>{content.intro}</p>
-        
-      </main>
+      <h2 className = "mb-4 text-3xl font-bold">{content.greeting}</h2>
+      <p className = "text-center text-lg text-gray-700">{content.intro}</p>
 
-    </div>
+    </main>
   );
 }
