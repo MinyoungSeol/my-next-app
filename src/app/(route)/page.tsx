edc1d13b13823907b2d758664164bcd9c5b0348d
MinyoungSeol <seol.ca06@gmail.com>
2025-09-23 11:04:47 -0400

    feat(wip): styling responsive to main page content

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -7,7 +7,7 @@ import RightSection from "@/components/RightSection";
 export default function Home() {
 
   return (
-    <main className = "mx-auto h-screen w-full max-w-7xl flex flex-row items-start justify-center px-4 pt-24 border border-red-200">
+    <main className = "mx-auto flex flex-col-reverse md:flex-row md:h-screen w-full items-start justify-center px-4 pt-24 border border-red-200">
 
       <LeftSection />
       <RightSection />
